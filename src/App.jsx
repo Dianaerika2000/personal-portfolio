28e2fc5dce041e1f04558bb670a6bcbc5e82d79b
Montaño Flores Diana Erika <dianaerika.montano16@gmail.com>
2025-08-01 13:15:38 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-purple-600">
+        Hello, Tailwind with React (using @tailwindcss/vite)!
+      </h1>
+    </>
+  )
+}
+
+export default App
